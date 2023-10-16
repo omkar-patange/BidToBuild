a145e17918f927ae8e2cff78a91a10e2e73b9c3b
tanmay-8 <113489283+tanmay-8@users.noreply.github.com>
2023-10-16 14:47:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+let basePrice = 0;
+let curBid = 0;
+let prevBid = 0;
+let previmg =  "";
+let basePriceInput = document.getElementById("basePrice");
+let curBidInput = document.getElementById("currentBid");
+let saveBtn = document.getElementById("save");
+
+const reset = () => {
+  basePrice = 0;
+  curBid = 0;
+  basePriceInput.value = 0;
+  curBidInput.value = 0;
+};
+
+const setCurBid = (newbid) => {
+    curBid = newbid;
+    curBidInput.value = curBid;
+}
+
+
+const  incrementCurBid = () => {   
+    let toIncrement = (curBid<200)?20:50;
+    let newbid = curBid + toIncrement;
+    setCurBid(newbid);
+}
+
+const undo=()=>{
+    document.getElementById("teamLogo").src = previmg;
+    setCurBid(prevBid);
+}
+
+reset();
+
+saveBtn.addEventListener("click", (e) => {
+  console.log("Base Price: ", basePrice);
+  console.log("Current Bid: ", curBid);
+  e.preventDefault();
+  reset();
+});
+
+basePriceInput.addEventListener("change", (e) => {
+    basePrice = parseInt(e.target.value);
+    setCurBid(basePrice);
+})
+
+document.addEventListener("keypress", (e) => {
+  if (
+    basePriceInput !== document.activeElement &&
+    curBidInput !== document.activeElement
+  ) {
+    let key = parseInt(e.key);
+    if (key >= 0 && key <= 9) {
+      previmg = document.getElementById("teamLogo").src;
+      prevBid = curBid;
+      document.getElementById("teamLogo").src = `./Images/team${key}.png`;
+      incrementCurBid();
+    }
+    if(e.key === "u")
+        undo();
+  }
+});
